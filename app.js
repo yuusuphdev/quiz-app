d1f1c6a4bac800f6f480548963245d2dd07655fe
yuusuphdev <134336794+yuusuphdev@users.noreply.github.com>
2024-05-07 09:53:03 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result')
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
+
+    //check answers
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            score += 25;
+        }
+    });
+
+    //show result on page
+    scrollTo(0,0);
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() =>{
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+        clearInterval(timer);
+        } else{
+            output++;
+
+        }
+
+    }, 10);
+});
+
+//window object
+// console.log('hello');
+// window.console.log('hello');
+
+//  console.log(document.querySelector('form'));
+//  console.log(window.document.querySelector('form'));
+
+// alert('hello');
+// window.alert('hello');
